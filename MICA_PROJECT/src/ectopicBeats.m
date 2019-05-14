5ade2fd6a4b5a7c85c31646451b8ab38192b00cd
Lucaazel <luca.zelmans@gmail.com>
2019-05-14 12:03:28 +0200

    structure du code et detection extracystol

---
+++ MICA_PROJECT/src/ectopicBeats.m
@@ -0,0 +1,25 @@
+function [ indices, deltas ] = ectopicBeats( R_loc, Fs )
+%Determined if the patient has ectopic beats or not
+%   return the indices of the ectopic beat(s)
+indices = [];
+deltas = [];                %All the interval between two consecutive R complex (in s)
+for i=1:length(R_loc)-1
+    delta = (R_loc(i+1) - R_loc(i))/Fs;
+    if (delta > 0.15)
+        deltas = [deltas delta];
+    else
+        deltas = [deltas mean(deltas)];
+    end
+end
+
+seuil =mean(deltas) - 0.01*(min(deltas))
+
+for j=1:length(deltas)-1
+    if deltas(j) >= seuil
+        indices = [indices j];
+    end
+end
+
+
+end
+
